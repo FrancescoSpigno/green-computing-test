fb390be38c1281521e7d2aaaeb705d4adb8195f1
Francesco Spigno <frandev@MBPdiFrancesco.station>
2023-12-20 15:06:03 +0100

    Bug Fix

--- projects/rust-rocket/src/main.rs
+++ projects/rust-rocket/src/main.rs
@@ -7,19 +7,19 @@ const PREFIX: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/");
 
 #[get("/small")]
 async fn small() -> Option<File> {
-    let filename = Path::new(PREFIX).parent()?.parent()?.join("file-test").join("small.json");
+    let filename = Path::new(PREFIX).join("file-test").join("small.json");
     File::open(&{filename}).await.ok()
 }
 
 #[get("/medium")]
 async fn medium() -> Option<File> {
-    let filename = Path::new(PREFIX).parent()?.parent()?.join("file-test").join("medium.json");
+    let filename = Path::new(PREFIX).join("file-test").join("medium.json");
     File::open(&{filename}).await.ok()
 }
 
 #[get("/large")]
 async fn large() -> Option<File> {
-    let filename = Path::new(PREFIX).parent()?.parent()?.join("file-test").join("large.json");
+    let filename = Path::new(PREFIX).join("file-test").join("large.json");
     File::open(&{filename}).await.ok()
 }
 
